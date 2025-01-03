60588da5ab31d52afdaa8c067e03852872c0454d
Afonso Neves <up202108884@up.pt>
2025-01-03 18:47:46 +0000

    utils docs

--- src/utils.pl
+++ src/utils.pl
@@ -7,8 +7,8 @@
 % https://stackoverflow.com/questions/53262099/swi-prolog-how-to-clear-terminal-screen-with-a-keyboard-shortcut-or-global-pre
 clear :- write('\33\[2J').
 
-% initial_board(-Board).
-% Creates the initial game board.
+% initial_board(-Board)
+% Creates the initial state board.
 initial_board(Board) :-
     Board = [
         [neutral, neutral, neutral, neutral, neutral],
@@ -23,15 +23,7 @@ initial_board(Board) :-
 count([], 0).
 count([H | T], N) :- count(T, N1), N is N1 + 1.
 
-% replace_row(Row, X, Piece, NewRow)
-% Replaces the piece in the X position of the given row.
-replace_row([], _, _, []).
-replace_row([Cell | Rest], 1, Piece, NewRow) :-
-    NewRow = [Piece | Rest].
-replace_row([Cell | Rest], X, Piece, NewRow) :-
-    NewX is X - 1,
-    replace_row(Rest, NewX, Piece, NewRest),
-    NewRow = [Cell | NewRest].
+/* Board Manipulation */
 
 % replace(+Board, +X, +Y, +Piece, -NewBoard)
 % Replaces the piece at the given coordinates. Y is list relative, not coordinate relative.
@@ -43,3 +35,13 @@ replace([Row | Rest], X, Y, Piece, NewBoard) :-
     NewY is Y - 1,
     replace(Rest, X, NewY, Piece, NewRest),
     NewBoard = [Row | NewRest].
+
+% replace_row(+Row, +X, +Piece, -NewRow)
+% Replaces the piece in the X position of the given row.
+replace_row([], _, _, []).
+replace_row([Cell | Rest], 1, Piece, NewRow) :-
+    NewRow = [Piece | Rest].
+replace_row([Cell | Rest], X, Piece, NewRow) :-
+    NewX is X - 1,
+    replace_row(Rest, NewX, Piece, NewRest),
+    NewRow = [Cell | NewRest].
