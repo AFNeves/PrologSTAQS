14c7d31cff11c1462b78038108c5b38c01ffa6de
Afonso Neves <up202108884@up.pt>
2025-01-04 21:52:41 +0000

    utils final

--- src/utils.pl
+++ src/utils.pl
@@ -21,29 +21,9 @@ dup_char(N, Char, Sequence) :-
     dup_char(N1, Char, Sequence1),
     atom_concat(Char, Sequence1, Sequence).
 
-% ----------------------------- %
-% Board Manipulation Predicates %
-% ----------------------------- %
-
-% initial_board(-Board)
-% Creates the initial state board.
-initial_board(Board) :-
-    Board = [
-        [neutral, neutral, neutral, neutral, neutral],
-        [neutral, neutral, neutral, neutral, neutral],
-        [neutral, neutral, neutral, neutral, neutral],
-        [neutral, neutral, neutral, neutral, neutral],
-        [neutral, neutral, neutral, neutral, neutral]
-    ].
-
-board_play_move(Board) :-
-    Board = [
-        [[blue, 1], neutral, neutral, [blue, 1], neutral],
-        [neutral, neutral, neutral, [white, 1], [white, 1]],
-        [neutral, neutral, [blue, 1], neutral, neutral],
-        [neutral, [white, 1], neutral, neutral, neutral],
-        [[blue, 1], neutral, neutral, neutral, [white, 1]]
-    ].
+% ------------------------- %
+% Piece Movement Predicates %
+% ------------------------- %
 
 % replace(+Board, +X, +Y, +Piece, -NewBoard)
 % Replaces the piece at the given coordinates. Y is list relative, not coordinate relative.
@@ -65,3 +45,40 @@ replace_row([Cell | Rest], X, Piece, NewRow) :-
     NewX is X - 1,
     replace_row(Rest, NewX, Piece, NewRest),
     NewRow = [Cell | NewRest].
+
+% ----------------------- %
+% Board Stages Predicates %
+% ----------------------- %
+
+% initial_board(-Board)
+% Creates the initial state board.
+initial_board(Board) :-
+    Board = [
+        [neutral, neutral, neutral, neutral, neutral],
+        [neutral, neutral, neutral, neutral, neutral],
+        [neutral, neutral, neutral, neutral, neutral],
+        [neutral, neutral, neutral, neutral, neutral],
+        [neutral, neutral, neutral, neutral, neutral]
+    ].
+
+% play_move_board(-Board)
+% Board used in the play_move\0 predicate.
+play_move_board(Board) :-
+    Board = [
+        [[blue, 1], neutral, neutral, [blue, 1], neutral],
+        [neutral, neutral, neutral, [white, 1], [white, 1]],
+        [neutral, neutral, [blue, 1], neutral, neutral],
+        [neutral, [white, 1], neutral, neutral, neutral],
+        [[blue, 1], neutral, neutral, neutral, [white, 1]]
+    ].
+
+% play_final_board(-Board)
+% Board used in the play_final\0 predicate.
+play_final_board(Board) :-
+    Board = [
+        [empty, [blue, 2], neutral, [blue, 1], neutral],
+        [empty, [blue, 5], neutral, empty, [white, 1]],
+        [empty, [blue, 2], empty, [white, 2], neutral],
+        [empty, empty, [white, 6], empty, neutral],
+        [empty, empty, empty, empty, [white, 1]]
+    ].
